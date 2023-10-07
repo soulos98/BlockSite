47ca67c3d0c90133319d7c36d7f407f844c9a312
soulos98 <jg03298@gmail.com>
2023-10-07 14:48:34 -0700

    Added some boiler plate to webpage

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) # __name__ is just the name of the module
 
+posts = [
+    {
+        'author': 'Jose A. Garcia',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted': 'October 7, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted': 'April 21, 2023'        
+    }
+]
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Hello World!<h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>This is my about page<h1>"
+    return render_template('about.html', title='about')
 
 if __name__ == '__main__':
     app.run(debug=True)
